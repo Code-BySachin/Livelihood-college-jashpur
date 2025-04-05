b80148e68c8b9285a6e4a78de589a5f080dd0980
sachin <sachin680125@gmail.com>
2025-04-05 22:25:54 +0530

    feat: add active link

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,17 +1,17 @@
 import logo from '../assets/logo.png'
-import { NavLink,Link } from 'react-router';
+import { NavLink, Link } from 'react-router-dom';
 function Navbar() {
     return (
 
         <nav className="flex  border-b-1">
             <span className='w-25 h-25'><img src={logo} alt="Logo" /></span>
             <ul className="flex justify-around w-300 py-8 hover:border-b-1">
-                <Link to={'/'} className=' hover:border-b-1 hover:text-blue-700'>Home</Link>        
-                <Link to={'/admission'} className=' hover:border-b-1 hover:text-blue-700'>Admissions</Link>
-                <Link to={'/faculties'} className=' hover:border-b-1 hover:text-blue-700'>Faculties</Link>
-                <Link to={'/course'} className=' hover:border-b-1 hover:text-blue-700'>Departments</Link>
-                <Link to={'/about'} className=' hover:border-b-1 hover:text-blue-700'>About</Link>
-                <Link to={'/contact'} className=' hover:border-b-1 hover:text-blue-700'>Contact</Link>
+                <NavLink to={'/'} className={({ isActive }) => isActive ? ' border-b-1 text-blue-700':' hover:border-b-1 hover:text-blue-700'}>Home</NavLink>
+                <NavLink to={'/admission'} className={({ isActive }) => isActive ? ' border-b-1 text-blue-700':' hover:border-b-1 hover:text-blue-700'}>Admissions</NavLink>
+                <NavLink to={'/faculties'} className={({ isActive }) => isActive ? ' border-b-1 text-blue-700':' hover:border-b-1 hover:text-blue-700'}>Faculties</NavLink>
+                <NavLink to={'/course'} className={({ isActive }) => isActive ? ' border-b-1 text-blue-700':' hover:border-b-1 hover:text-blue-700'}>Departments</NavLink>
+                <NavLink to={'/about'} className={({ isActive }) => isActive ? ' border-b-1 text-blue-700':' hover:border-b-1 hover:text-blue-700'}>About</NavLink>
+                <NavLink to={'/contact'} className={({ isActive }) => isActive ? ' border-b-1 text-blue-700':' hover:border-b-1 hover:text-blue-700'}>Contact</NavLink>
             </ul>
         </nav>
     )
