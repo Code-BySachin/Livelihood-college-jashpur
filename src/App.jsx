d1fe8fba5c5fa1b34efbc76c6646b460a60b403b
sachin <sachin680125@gmail.com>
2025-04-14 22:28:45 +0530

    Fix: Routers are pushed to App.jsx from Home.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,33 @@
 import Home from "./Components/Home"
+import Header from "./Components/Header"
+import Navbar from "./Components/Navbar"
+import Footer from "./Components/Footer";
+import Admission from "./Components/Admission";
+import AboutUS from "./Components/AboutUs";
+import Contact from "./Components/Contact";
+import AdmissionForm from "./Components/AdmissionForm";
+import Course from "./Components/Course";
+import StaffPage from "./Components/StaffPage";
+import { BrowserRouter, Routes, Route } from 'react-router'
 
 function App() {
   return(
-    <Home/>
+    <BrowserRouter>
+    <Header />
+    <Navbar />
+    <Routes>
+        <Route path='/' element={<Home/> } />
+        <Route path='/admission' >
+            <Route index element={<Admission />} />
+            <Route path="apply" element={<AdmissionForm/>}/>
+        </Route>
+        <Route path="/faculties" element={<StaffPage />} />
+        <Route path="schemes/course" element={<Course />} />
+        <Route path='/contact' element={<Contact />} />
+        <Route path='/about' element={<AboutUS />} />
+    </Routes>
+    <Footer />
+</BrowserRouter>
   )
 }
 
