ab8643070a7603574158a5f1a9942f63e6369889
sachin <sachin680125@gmail.com>
2025-04-15 15:52:46 +0530

    refactor: removed unnecessary code

--- Admin/src/App.jsx
+++ Admin/src/App.jsx
@@ -1,35 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import AdminDashboard from "./AdminDashboard";
+import UpdateCourses from "./components/UpdateCourses";
+import UpdateFaculties from "./components/UpdateFaculties";
+import UpdateCarousel from "./components/UpdateCarousel";
+import UpdateLatest from "./components/UpdateLatest";
+import UpdateSchemes from "./components/UpdateSchemes";
+import UpdateFaqs from "./components/UpdateFaqs";
+import UpdateAdmission from "./components/UpdateAdmission";
+// Import the rest...
+function App(){
+  return(
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <BrowserRouter>
+  <Routes>
+    <Route path="/admin" element={<AdminDashboard />} />
+    <Route path="/admin/updatelatest" element={<UpdateLatest />} />
+    <Route path="/admin/updatecarousel" element={<UpdateCarousel />} />
+    <Route path="/admin/updatecourses" element={<UpdateCourses />} />
+    <Route path="/admin/updateadmission" element={<UpdateAdmission />} />
+    <Route path="/admin/updateschemes" element={<UpdateSchemes />} />
+    <Route path="/admin/updatefaculties" element={<UpdateFaculties />} />
+    <Route path="/admin/updatefaqs" element={<UpdateFaqs />} />
+    {/* Add other admin routes here */}
+  </Routes>
+</BrowserRouter>
   )
 }
-
-export default App
+export default App;
