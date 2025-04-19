8d514c30bfc57a16b2403ec3860e1b1f58a15d7e
sachin <sachin680125@gmail.com>
2025-04-19 15:51:11 +0530

    Changed navbar look

--- Frontend/src/Components/Header.jsx
+++ Frontend/src/Components/Header.jsx
@@ -1,9 +1,13 @@
+import logo from '../assets/logo.png'
+import livelihood_logo from '../assets/jashpur-livelihood-logo.png'
 import emailIcon from '../assets/Icons/email.png'
 import locationIcon from '../assets/Icons/placeholder.png'
 function Header() {
     return (
-        <header className="bg-blue-800 py-2 flex justify-between">
-            <p className="px-5 text-white"><img className='w-5' src={locationIcon} alt="*" />GOVERNMENT LIVLIHOOD COLLEGE JASHPUR</p>
+        <header className="bg-blue-800 py-2 flex justify-center items-center">
+            <span className='w-25 h-25'><img src={logo} alt="Logo" /></span>
+            <h1 className="px-5 text-white text-3xl ">GOVERNMENT LIVLIHOOD COLLEGE JASHPUR</h1>
+            <span className='w-25 h-25 rounded-full overflow-hidden'><img src={livelihood_logo} alt="Logo"/></span>
             {/* <p className="px-5">logo</p> */}
         </header>
 
--- Frontend/src/Components/Navbar.jsx
+++ Frontend/src/Components/Navbar.jsx
@@ -1,10 +1,8 @@
-import logo from '../assets/logo.png'
 import { NavLink, Link } from 'react-router-dom';
 function Navbar() {
     return (
 
         <nav className="flex  border-b-1">
-            <span className='w-25 h-25'><img src={logo} alt="Logo" /></span>
             <ul className="flex justify-around w-300 py-8 hover:border-b-1">
                 <NavLink to={'/'} className={({ isActive }) => isActive ? ' border-b-1 text-blue-700' : ' hover:border-b-1 hover:text-blue-700'}>Home</NavLink>
                 <NavLink to={'/admission'} className={({ isActive }) => isActive ? ' border-b-1 text-blue-700' : ' hover:border-b-1 hover:text-blue-700'}>Admissions</NavLink>
