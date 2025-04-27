2715792748910110300dfc65e55eda95e3deeb21
sachin <sachin680125@gmail.com>
2025-04-27 11:45:12 +0530

    add heading of jashpur

--- Admin/src/AdminDashboard.jsx
+++ Admin/src/AdminDashboard.jsx
@@ -13,7 +13,8 @@ const adminSections = [
 const AdminDashboard = () => {
   return (
     <div className="min-h-screen bg-gray-100 p-8">
-      <h1 className="text-3xl font-bold mb-6 text-center text-blue-800">Admin Dashboard</h1>
+      <h1 className="text-3xl font-bold mb-6 text-center">Livelihood college Jashpur</h1>
+      <h2 className="text-3xl font-bold mb-6 text-center text-blue-800">Admin Dashboard</h2>
 
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 max-w-5xl mx-auto">
         {adminSections.map((section, index) => (
